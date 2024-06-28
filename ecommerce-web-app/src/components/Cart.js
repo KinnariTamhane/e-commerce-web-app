07e8cc5d0ba9f29f03f9754c2d3db29305ec79ba
Kinnari <kinnari.tamhane@tickledmedia.com>
2024-06-28 17:50:28 +0530

    completed e-commerce app

--- ecommerce-web-app/src/components/Cart.js
+++ ecommerce-web-app/src/components/Cart.js
@@ -12,16 +12,19 @@ const Cart = () => {
 
   return (
     <div className='mb-10'>
-      <h1 className="text-2xl font-bold mb-10 pl-3 pt-3 text-center mt-8">Shopping Cart</h1>
+      <div className='text-center'>
+        <h1 className="text-2xl font-bold pb-5 pt-4 mt-10">Shopping Cart</h1>
+      </div>
       {cartItems.length === 0 ? (
         <p className='mt-40 mb-20 text-center flex justify-center align-middle min-h-[70vh]'>Your cart is empty.</p>
       ) : (
         <div>
+          <p className='text-lg text-center'>Total Items : {cartItems.length}</p>
           {cartItems.map((item) => (
             <div key={item.id} className="border rounded-lg  shadow-md mb-4 p-5 m-5">
             <div>
                 <div className='w-[150px] mx-auto my-0'>
-                    <img src={item.images} alt={item.title}/>
+                    <img src={item.thumbnail} alt={item.title}/>
                 </div>
                 <div>
                     <h2 className="text-xl font-bold mb-2">{item.title}</h2>
